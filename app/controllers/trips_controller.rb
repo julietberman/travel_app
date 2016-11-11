6033e9b8776fee029d273c7eb62c7905d30fae18
Juliet Berman <berman.juliet@gmail.com>
2016-11-11 15:43:57 -0500

    basic CRUD functionality for trip model

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,4 +6,40 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
   end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    trip = Trip.find(params[:id])
+    trip.update(trip_params)
+
+    redirect_to trip_path(trip)
+  end
+
+
+  def new
+    @trip= Trip.new
+  end
+
+  def create
+    trip = Trip.new(trip_params)
+    trip.save
+
+    redirect_to trips_path
+  end
+
+  def destroy
+    trip= Trip.find(params[:id])
+    trip.destroy
+
+    redirect_to trips_path
+  end
+
+  private
+  def trip_params
+    params.require(:trip).permit(:country, :trip, :departure, :return, :availability, :img_url, :cost, :description)
+  end
+
 end
