c88e0abfb42d922c24670f3996f203d93c023b90
Juliet Berman <berman.juliet@gmail.com>
2016-11-16 15:04:52 -0500

    fixed bug

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
 
+
 def new
     @profile = current_user.build_profile
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    '/profiles/new' 
+    '/profiles/new'
   end
 end
